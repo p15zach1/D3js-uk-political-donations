298c87036f1fd0bb9e32b4c2c6b06b1d4a35b8a5
p15zach1 <zaxalexander@hotmail.com>
2018-02-28 19:34:15 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -48,12 +48,14 @@ function transition(name) {
 		$("#view-donor-type").fadeOut(250);
 		$("#view-source-type").fadeOut(250);
 		$("#view-party-type").fadeOut(250);
+		$("#view-by-amount").fadeOut(250);
 		return total();
 		//location.reload();
 	}
 	if (name === "group-by-party") {
 		$("#initial-content").fadeOut(250);
 		$("#value-scale").fadeOut(250);
+		$("#view-by-amount").fadeOut(250);
 		$("#view-donor-type").fadeOut(250);
 		$("#view-source-type").fadeOut(250);
 		$("#view-party-type").fadeIn(1000);
@@ -62,13 +64,24 @@ function transition(name) {
 	if (name === "group-by-donor-type") {
 		$("#initial-content").fadeOut(250);
 		$("#value-scale").fadeOut(250);
+		$("#view-by-amount").fadeOut(250);
 		$("#view-party-type").fadeOut(250);
 		$("#view-source-type").fadeOut(250);
 		$("#view-donor-type").fadeIn(1000);
 		return donorType();
 	}
+	if (name === "group-by-amount") {
+		$("#initial-content").fadeOut(250);
+		$("#value-scale").fadeOut(250);
+		$("#view-donor-type").fadeOut(250);
+		$("#view-party-type").fadeOut(250);
+		$("#view-source-type").fadeOut(250);
+		$("#view-by-amount").fadeIn(1000);
+		return amountSort();
+	}
 	if (name === "group-by-money-source")
 		$("#initial-content").fadeOut(250);
+		$("#view-by-amount").fadeOut(250);
 		$("#value-scale").fadeOut(250);
 		$("#view-donor-type").fadeOut(250);
 		$("#view-party-type").fadeOut(250);
@@ -135,6 +148,14 @@ function donorType() {
 		.start();
 }
 
+function amountSort() {
+	force.gravity(0)
+		.friction(0.8)
+		.charge(function(d) { return -Math.pow(d.radius, 2.0) / 3; })
+		.on("tick", byAmount)
+		.start();
+}
+
 function fundsType() {
 	force.gravity(0)
 		.friction(0.75)
@@ -157,6 +178,13 @@ function entities(e) {
 			.attr("cy", function(d) {return d.y; });
 }
 
+function byAmount(e) {
+	node.each(moveTobyAmount(e.alpha));
+
+		node.attr("cx", function(d) { return d.x; })
+			.attr("cy", function(d) {return d.y; });
+}
+
 function types(e) {
 	node.each(moveToFunds(e.alpha));
 
@@ -172,7 +200,6 @@ function all(e) {
 		node.attr("cx", function(d) { return d.x; })
 			.attr("cy", function(d) {return d.y; });
 }
-
 
 function moveToCentre(alpha) {
 	return function(d) {
@@ -223,6 +250,36 @@ function moveToEnts(alpha) {
 
 		d.x += (centreX - d.x) * (brake + 0.02) * alpha * 1.1;
 		d.y += (centreY - d.y) * (brake + 0.02) * alpha * 1.1;
+	};
+}
+
+function moveTobyAmount(alpha) {
+	return function(d) {
+			var centreX;
+			var centreY;
+			if (d.value <= 100000) {
+				centreY = 700;
+				centreX = 300;
+				
+			} else if (d.value <= 500000) {
+				centreY = 600;
+				centreX = 750;
+				
+			} else if (d.value <= 1000000) {
+				centreY = 500;
+				centreX = 300;
+				
+			} else  if (d.value <= 5000000) {
+				centreY = 400;
+				centreX = 750;
+				
+			} else  if (d.value <= maxVal) {
+				centreY = 300;
+				centreX = 300;
+			}
+
+		d.x += (centreX - d.x) * (brake + 0.06) * alpha * 1.2;
+		d.y += (centreY - 100 - d.y) * (brake + 0.06) * alpha * 1.2;
 	};
 }
 
@@ -328,6 +385,8 @@ function mouseover(d, i) {
     .style("top", (parseInt(d3.select(this).attr("cy") - (d.radius+150)) + offset.top) + "px")
 		.html(infoBox)
 			.style("display","block");
+	var speech = new SpeechSynthesisUtterance("The donator is " + donor + " and the given amount is " + amount + " pounds");
+	window.speechSynthesis.speak(speech);
 	}
 
 function mouseout() {
@@ -338,6 +397,7 @@ function mouseout() {
 
 		d3.select(".tooltip")
 			.style("display", "none");
+		window.speechSynthesis.cancel();
 		}
 
 $(document).ready(function() {
